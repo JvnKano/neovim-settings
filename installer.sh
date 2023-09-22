ba0c6c5209c48e4c2b65803d84a956721f4c7a68
Jvn Kano <tonotyan23@gmail.com>
2023-09-23 01:25:13 +0900

    version 2

--- installer.sh
+++ installer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ ! -d ~/.config] ; then
+if [ ! -d ~/.config ] ; then
    mkdir ~/.config
 fi
 
-if [ ! -d ~/.config/nvim] ; then
+if [ ! -d ~/.config/nvim ] ; then
    mkdir ~/.config/nvim
 fi
 
 
-if [ ! -d ~/.config/nvim/lua] ; then
+if [ ! -d ~/.config/nvim/lua ] ; then
    mkdir ~/.config/nvim/lua
 fi
 
