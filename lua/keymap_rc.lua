abad471b7ec1d563fd56a9326198098f4f1a2e0b
JvnKano <tonotyan23@gmail.com>
2024-06-14 22:59:50 +0900

    [Add] CopenOnOff()

--- lua/keymap_rc.lua
+++ lua/keymap_rc.lua
@@ -44,6 +44,7 @@ map("n", "<c-p>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", options)
 map("n", "<c-n>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", options)
 map({"c", "i"}, "<c-r><c-r>", "\"", {noremap = true})
 --map("n", "R", ":FlutterReload<cr>", options)
+map("n", "<a-q>", ":lua CopenOnOff()<cr>", options)
 map("n", "<a-5>", ":lua HlsearchOnOff()<cr>", options)
 map("n", "]q", "<cmd>cn<cr>", options)
 map("n", "[q", "<cmd>cp<cr>", options)
@@ -51,6 +52,10 @@ map("", "<leader>\\", "<cmd>lua require'hop'.hint_words()<cr>", options)
 map("n", "<leader>e", "<cmd>w<CR><cmd>make<CR>", options);
 map("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", options);
 map("n", "<leader>he", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", options);
+-- map("n", "<a-h>", "<c-w><", options);
+-- map("n", "<a-l>", "<c-w>>", options);
+-- map("n", "<a-j>", "<c-w>+", options);
+-- map("n", "<a-k>", "<c-w>-", options);
 
 --map("n", "<leader>w", ":w<cr>", options);
 
@@ -59,6 +64,7 @@ map("n", "<leader>l", "<cmd>loadview<CR>", options);
 map("n", "*", "<cmd>let @/='\\<'.expand('<cword>').'\\>'<cr>", options);
 --map("n", "<c-s>", "<cmd>Fern . -drawer -toggle<CR>", options);
 map("x", "<leader>p", "\"_dP", options); --map("n", "<c-s>", "<cmd>Fern %:h -drawer -toggle<CR>", options); map("n", "<F7>", "<cmd>DapToggleRepl<CR>", options);
+
 --map("n", "<F8>", "<cmd>DapContinue<CR>", options);
 --map("n", "<F9>", "<cmd>DapStepOver<CR>", options);
 --map("v","<M-k>","<Cmd>lua require('dapui').eval()<CR>", {})
