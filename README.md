0a2d5e08f057a597124a054ec78de44d3a20437f
JvnKano <tonotyan23@gmail.com>
2024-06-25 00:11:15 +0900

    [Modify] init.lua

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 if some of your icon in floating window of completion doesn't display correctly, you should install Nerd Fonts or disabling the showing icon function in nvimcmp_rc.lua file
 
-
+if html server doesn't work, you should upgrade nodejs to lts version
--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require("lazy_rc")
 require("options")
 require("telescope_rc")
 require("mason").setup()
+require("mason_lspconfig_rc")
 require("lspconfig_rc")
 require("keymap_rc")
 require("nvimcmp_rc")
@@ -19,11 +20,18 @@ require("oil_rc")
 require("treesj_rc")
 require("ufo").setup()
 require("nvim_ufo_rc")
+--require("lualine_rc")
+--require("pomo_rc")
+--require('overseer').setup()
 require("tokyonight").setup({
    transparent = false,
-   styles = { sidebars = "transparent",
+   styles =
+   {
+      sidebars = "transparent",
       floats = "transparent",
-   }, }) vim.cmd [[
+   },
+})
+vim.cmd [[
 augroup InitLua
 au!
 au BufWritePost ~/.config/nvim/init.lua bufdo source ~/.config/nvim/init.lua
